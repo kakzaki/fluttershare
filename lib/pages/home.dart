19c8a964c93308d68fac573f8edbf942f091e2e4
Jesse Belcourt <jessebelcourt@jessebelcourt.hsd1.co.comcast.net>
2019-08-01 14:30:52 -0600

    Added a splash file to the the 'home' view.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,8 +7,59 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _isAuth = false;
+
+  Widget _buildAuthScreen() {
+    return Text('Authenticated');
+  }
+
+  Scaffold _buildUnAuthScreen() {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.teal,
+              Colors.purple,
+            ],
+          )
+        ),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'FlutterShare',
+              style: TextStyle(
+                fontFamily: "Signatra",
+                fontSize: 90.0,
+                color: Colors.white,
+              ),
+            ),
+            GestureDetector(
+              onTap: () =>  print("this worked..."),
+              child: Container(
+                width: 260.0,
+                height: 60.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/google_signin_button.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Home");
+    return _isAuth ? _buildAuthScreen() : _buildUnAuthScreen();
   }
 }
